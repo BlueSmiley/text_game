597979c75cadb3606952404276929706495c3f59
BlueSmiley <BlueSmiley@users.noreply.github.com>
2017-07-18 00:10:03 +0100

    made button input arrive only in selected line

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 $(function(){
+ var currentLine;
+ 
+ $("span.sentence").dblclick(funtion(event){
+     currentLine = event.target.id;                  
+  });
+ 
  $("button").click(function(event){
-  $("span").children("span.blanks").text($(event.target).text());
+  if(currentLine){
+   $("#" + currentLine).children(".blanks").text($(event.target).text());
+  }
  });
 });
