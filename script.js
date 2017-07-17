e999e14ea4d0a325cf7a7988ef16a4894d2a2a6c
BlueSmiley <BlueSmiley@users.noreply.github.com>
2017-07-18 00:39:18 +0100

    added a color change

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 $(function(){
  var currentLine;
  
- $("span.sentence").dblclick(function(event){
-     currentLine = event.target.id;                  
+ $("span.sentence").click(function(event){
+     if(currentLine){
+      $("#" + currentLine).css("color","black");
+     }
+     currentLine = event.target.id;
+     $("#" + currentLine).css("color","red");
   });
  
  $("button").click(function(event){
